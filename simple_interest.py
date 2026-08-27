3375802ca13fd39f0d0fd569280b08091622e119
Manju Prasad B M <manjuprasad31sky@gmail.com>
2026-08-27 21:49:28 +0530

    uploaded from VS code

---
+++ simple_interest.py
@@ -0,0 +1,6 @@
+p=int(input("enter the principle amount: "))
+r=int(input("enter the rate of interest: "))
+t=int(input("enter the time period: "))
+r=r*0.01
+priciple_interest=p*r*t
+print("your principle interest is: ", priciple_interest)
